74d902249a6612689a203cfa674f4797adc548db
trepvox <smylesmoriarty@gmail.com>
2021-09-15 18:54:39 -0400

    git: Merge broken/head files, etc.

---
+++ src/main/java/com/saxyrepairtracker/saxophone/entity/Saxophones.java
@@ -0,0 +1,16 @@
+package com.saxyrepairtracker.saxophone.entity;
+
+public class Saxophones {
+
+  private int saxophonePK;
+  
+  private int customerFK;
+  
+  private int serialNumber;
+  
+  private String manufacturer;
+  
+  private String series;
+  
+  private enum type;
+}
