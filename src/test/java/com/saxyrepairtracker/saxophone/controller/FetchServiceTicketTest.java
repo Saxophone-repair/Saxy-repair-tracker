e2a80079c875b9983b95bf331079630072d99484
JoleneMel <jolenemelanson33@gmail.com>
2021-09-15 15:35:25 -0400

    added packages

---
+++ src/test/java/com/saxyrepairtracker/saxophone/controller/FetchServiceTicketTest.java
@@ -0,0 +1,13 @@
+package com.saxyrepairtracker.saxophone.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class FetchServiceTicketTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
