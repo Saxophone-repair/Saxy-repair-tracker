a204c0170a8d42f4a1b674a17e4b5cf972ffb546
Jolene <jolenemelanson33@gmail.com>
2021-09-16 14:00:59 -0400

    controllers added

--- src/main/java/com/saxyrepairtracker/saxophone/controller/DefaultEmployeeController.java
+++ src/main/java/com/saxyrepairtracker/saxophone/controller/DefaultEmployeeController.java
@@ -1,5 +1,15 @@
 package com.saxyrepairtracker.saxophone.controller;
 
-public class DefaultEmployeeController {
+
+import java.util.List;
+import com.saxyrepairtracker.saxophone.entity.Employee;
+
+public class DefaultEmployeeController implements EmployeeController{
+
+  @Override
+  public List<Employee> fetchEmployees(String firstName, String lastName) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
