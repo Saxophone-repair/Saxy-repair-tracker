93f64af59dc1862e0208be6c301c9b9b63cf31bc
Jolene <jolenemelanson33@gmail.com>
2021-09-16 14:15:15 -0400

    Employee entity

--- src/main/java/com/saxyrepairtracker/saxophone/entity/Employee.java
+++ src/main/java/com/saxyrepairtracker/saxophone/entity/Employee.java
@@ -1,5 +1,19 @@
 package com.saxyrepairtracker.saxophone.entity;
 
-public class Employee {
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+  private int employeePk;
+  private String firstName;
+  private String lastName;
+  private BigDecimal payRate;
 }
+
