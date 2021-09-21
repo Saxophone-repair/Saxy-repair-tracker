c11a4a21bbaae8338d346525b7632cd036515039
Jolene <jolenemelanson33@gmail.com>
2021-09-21 19:52:52 -0400

    Adjusted the notes wanted to push immediately since that might cause merge conflicts if we are both using it

--- src/main/java/com/saxyrepairtracker/notes.java
+++ src/main/java/com/saxyrepairtracker/notes.java
@@ -2,12 +2,18 @@ package com.saxyrepairtracker;
 
 public class notes {
   //Non-testing ways of implementations 
-//Controller interface first -api swagger, url looks like etc
+//Controller interface first -api swagger, url looks like etc 
+  //Employees (X)
   //controller class 
+//Employees ( )
   //service interface - list of functions, "business logic" (minimal in here tho) 
+//Employees ( )
   //service class -passes methods to dao 
+//Employees ( )
   //dao interface-list of functions 
+//Employees ( )
   //dao class - where sql lives 
+//Employees ( )
   
   
   //For each function we need those to be populated with it 
