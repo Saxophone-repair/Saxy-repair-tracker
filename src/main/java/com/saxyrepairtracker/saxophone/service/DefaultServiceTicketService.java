0b3dc0746e6e3ae0495503ae1da32b5ad43bd26b
Jolene <jolenemelanson33@gmail.com>
2021-09-23 19:25:03 -0400

    Added Dao layers, and starting to finish on the service layer class

--- src/main/java/com/saxyrepairtracker/saxophone/service/DefaultServiceTicketService.java
+++ src/main/java/com/saxyrepairtracker/saxophone/service/DefaultServiceTicketService.java
@@ -2,11 +2,21 @@ package com.saxyrepairtracker.saxophone.service;
 
 import java.util.List;
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.saxyrepairtracker.saxophone.dao.ServiceTicketDao;
 import com.saxyrepairtracker.saxophone.entity.ServiceTicket;
 import com.saxyrepairtracker.saxophone.entity.Status;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Service
+@Slf4j
 public class DefaultServiceTicketService implements ServiceTicketService{
 
+  @Autowired
+  private ServiceTicketDao serviceTicketDao;
+  
   @Override
   public List<ServiceTicket> fetchServiceTicketByStatus(Status status) {
     // TODO Auto-generated method stub
